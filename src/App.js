85d5e943a4b8753a850f5c29166e7355ddd68aad
Eden Brekke <eden.lorrai@gmail.com>
2023-02-09 11:58:37 -0800

    import topbar and render it in main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { ColorModeContext, useMode } from './theme'; // import the functions we just created
 import { CssBaseline, ThemeProvider } from '@mui/material'; // resets standard css to nothing, and allows us to use MUIs theme provider
 
+import Topbar from './scenes/global/Topbar';
+
 
 function App() {
   const [theme, colorMode] = useMode(); // get access the the theme and colorMode we created
@@ -10,7 +12,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className='content'></main>
+          <main className='content'>
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
