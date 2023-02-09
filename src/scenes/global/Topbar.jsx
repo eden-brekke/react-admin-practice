f2f1be9d6472eb4da34d64e8062842474ecb2ac8
Eden Brekke <eden.lorrai@gmail.com>
2023-02-09 11:59:23 -0800

    set up constants for theme grabbing and changing

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -12,6 +12,10 @@ import SearchIcon  from '@mui/icons-material/Search';
 
 
 const TopBar = () => {
+  const theme = useTheme(); // grab from material UI, does in react context but material ui so we have access to their theme provided
+  const colors = tokens(theme.palette.mode); // anytime that we want to use the color mode that is in material UI we can grab it from useTheme, and we can pass it in the tokens theme we created in our other file. so every time we want to use a color it will determine which color we want based on light vs dark mode
+  const colorMode = useContext(ColorModeContext); // allow us to toggle different states for the color mode
+  return (
 
   )
 }
