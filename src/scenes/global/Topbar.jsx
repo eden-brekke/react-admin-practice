0e43989a389beb875a8059a8193b5287732b41e1
Eden Brekke <eden.lorrai@gmail.com>
2023-02-09 12:25:57 -0800

    added search bar and icons, added notification, person, settings and light/dark mode icons and made sure that light/dark mode toggles correctly

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,23 +1,64 @@
-import React from 'react'
-import { Box, IconButton, useTheme } from '@mui/material';
-import { useContext } from 'react';
-import { ColorModeContext, tokens } from '../../theme';
-import InputBase from '@mui/material';
-import LightModeOutlinedIcon  from '@mui/icons-material/LightModeOutlined';
-import DarkModeOutlinedIcon  from '@mui/icons-material/DarkModeOutlined';
-import NotificationsOutlinedIcon  from '@mui/icons-material/NotificationsOutlined';
-import SettingsOutlinedIcon  from '@mui/icons-material/SettingsOutlined';
-import PersonOutlinedIcon  from '@mui/icons-material/PersonOutlined';
-import SearchIcon  from '@mui/icons-material/Search';
-
+import React from "react";
+import { Box, IconButton, useTheme, InputBase } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 const TopBar = () => {
   const theme = useTheme(); // grab from material UI, does in react context but material ui so we have access to their theme provided
   const colors = tokens(theme.palette.mode); // anytime that we want to use the color mode that is in material UI we can grab it from useTheme, and we can pass it in the tokens theme we created in our other file. so every time we want to use a color it will determine which color we want based on light vs dark mode
   const colorMode = useContext(ColorModeContext); // allow us to toggle different states for the color mode
   return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase
+          sx={{
+            ml: 2,
+            flex: 1,
+          }}
+          placeholder="Search"
+        />
+        <IconButton
+          type="button"
+          sx={{
+            p: 1,
+          }}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Box>
 
-  )
-}
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === 'dark' ? (
+            <LightModeOutlinedIcon />
+            ) : (
+            <DarkModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
 
-export default TopBar
+export default TopBar;
