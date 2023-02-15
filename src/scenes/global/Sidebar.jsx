15ee2aee01f042a736b607aaa0713d2c8ba8b89a
Eden Brekke <eden.lorrai@gmail.com>
2023-02-15 12:50:28 -0800

    added separators

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -131,6 +131,15 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
+            <Typography
+              variant='h6'
+              colors={colors.grey[300]}
+              sx={{
+                m: '15px 0 5px 20px'
+              }}
+            >
+              Data
+            </Typography>
             <Item
               title="Manage Team"
               to="/team"
@@ -152,6 +161,15 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
+            <Typography
+              variant='h6'
+              colors={colors.grey[300]}
+              sx={{
+                m: '15px 0 5px 20px'
+              }}
+            >
+              Pages
+            </Typography>
             <Item
               title="Profile Form"
               to="/form"
@@ -173,6 +191,15 @@ const Sidebar = () => {
               selected={selected}
               setSelected={setSelected}
             />
+            <Typography
+              variant='h6'
+              colors={colors.grey[300]}
+              sx={{
+                m: '15px 0 5px 20px'
+              }}
+            >
+              Charts
+            </Typography>
             <Item
               title="Bar Chart"
               to="/bar"
