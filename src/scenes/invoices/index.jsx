bfd9a4f55641c0b20f70fc630f2ebced64feef09
Eden Brekke <eden.lorrai@gmail.com>
2023-02-16 12:26:59 -0800

    added and styled checkboxes to invoice table

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -73,12 +73,13 @@ const Invoices = () => {
             borderTop: 'none',
             backgroundColor: colors.blueAccent[700] 
           },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-            color: `${colors.grey[100]} !important`,
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`
           }
         }}
       >
         <DataGrid 
+          checkboxSelection
           rows={mockDataInvoices} 
           columns={columns} 
         />
