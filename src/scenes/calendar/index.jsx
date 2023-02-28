790da3e76ff58c19cff750216cd0f7fa545b46c3
Eden Brekke <eden.lorrai@gmail.com>
2023-02-27 20:04:47 -0800

    added imports for calendar

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { useState } from 'react';
+import FullCalendar, { formatDate } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from "@fullcalendar/interaction";
+import listPLugin from '@fullcalendar/list';
 
 const Calendar = () => {
   return <div>Calendar</div>;
