d0a2f19122e7cb12cf28c6d11ab20448ae423b79
Eden Brekke <eden.lorrai@gmail.com>
2023-02-28 12:12:02 -0800

    added some typographies and list items

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useState } from "react";
-import FullCalendar, { formatDate } from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
+import { formatDate } from '@fullcalendar/core';
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import listPLugin from "@fullcalendar/list";
+import listPlugin from "@fullcalendar/list";
 import {
   Box,
   List,
@@ -18,8 +19,8 @@ import { tokens } from "../../theme";
 
 const Calendar = () => {
   const theme = useTheme();
-  const colors = tokens(theme.pallete.mode);
-  const [currentEvens, setCurrentEvents] = useState([]);
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
 
   const handleDateClick = (selected) => {
     const title = prompt("Please enter a new title for your event");
@@ -41,7 +42,48 @@ const Calendar = () => {
       selected.event.remove();
     }
   } // add a function when you click the event, it will give you the prompt
-  return <Box> Calendar </Box>;
+  return (
+    <Box m='20px'> 
+      <Header title='CALENDAR' subtitle="Full Calendar Interactive Page"/>
+      <Box display='flex' justifyContent='space-between'>
+        {/* Calendar Sidebar  */}
+        {/* flex style shorhand: grow, shink, and width or percentage of space it wants to take */}
+        <Box flex='1 1 20%'
+        backgroundColor={colors.primary[400]}
+        p='15px'
+        borderRadius='4px'> 
+          <Typography variant='h5'>
+            Events
+          </Typography>
+          <List>
+            {currentEvents.map((event)=> (
+              <ListItem 
+                key={event.id} 
+                sx={{
+                  backgroundColor: colors.greenAccent[500], 
+                  margin: '10px 0', 
+                  borderRadius:'2px'
+                }}>
+                  <ListItemText
+                    primary={event.title}
+                    secondary={
+                      <Typography>
+                        {/* formatDate is a function provided by fullCalendar allows you to use the dates given and format them */}
+                        {formatDate(event.start, {
+                          year: 'numeric',
+                          month: 'short',
+                          day: 'numeric',
+                        })}
+                      </Typography>
+                    }
+                  />
+                </ListItem>
+              ))}
+          </List>
+        </Box>
+      </Box>
+    </Box>
+    )
 };
 
 export default Calendar;
