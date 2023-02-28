696c3674f10c5492a864cad24d0acfc5a465466e
Eden Brekke <eden.lorrai@gmail.com>
2023-02-28 15:16:11 -0800

    accordion works

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,17 +1,64 @@
-import React from 'react'
-import { Box, useTheme, Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
-import { ExpandMoreIcon } from '@mui/icons-material';
+import React from "react";
+import {
+  Box,
+  useTheme,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { tokens } from "../../theme";
-import Header from '../../components/Header';
+import Header from "../../components/Header";
 
 const Faq = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    return (
-        <Box m='20px'>
-            <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
-        </Box>
-    )
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>An answer to the important question</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>An answer to the important question</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            A Third Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>An answer to the important question</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            A Final Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>An answer to the important question</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+};
 
-export default Faq
+export default Faq;
