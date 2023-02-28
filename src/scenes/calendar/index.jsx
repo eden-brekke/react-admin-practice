f57a5800149a31072a02ce7a26d32aecfbaa0833
Eden Brekke <eden.lorrai@gmail.com>
2023-02-28 11:37:53 -0800

    getting functions set up for calendar interactions

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,23 +1,47 @@
 import React from "react";
-import { useState } from 'react';
-import FullCalendar, { formatDate } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import listPLugin from '@fullcalendar/list';
+import listPLugin from "@fullcalendar/list";
 import {
-  Box, List, ListItem, ListItemText, Typography, useTheme } from '@mui/material';
-import Header from '../../components/Header';
-import { tokens } from '../../theme';
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
 
 const Calendar = () => {
   const theme = useTheme();
   const colors = tokens(theme.pallete.mode);
   const [currentEvens, setCurrentEvents] = useState([]);
 
-  return (
-    <Box> Calendar </Box>
-  )
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect(); // once we click on a date we unselect immediately so we can enter the title for the event instead
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      }); // add the event into the calendar app
+    }
+  }; // typically you'd create a modal instead with a check to open the modal, we're using the native JS prompt just to keep it simple
+
+  const handleEventClick = (selected) => {
+    if(window.confirm(`Are you sure you want to delete the event '${selected.event.title}'`)) {
+      selected.event.remove();
+    }
+  } // add a function when you click the event, it will give you the prompt
+  return <Box> Calendar </Box>;
 };
 
 export default Calendar;
