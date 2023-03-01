2059a33b53f0b886b8a67b9e92d4762a051112d8
Eden Brekke <eden.lorrai@gmail.com>
2023-02-28 19:58:21 -0800

    finished up adding conditional rendering for dashborad

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@mui/material';
-import { ResponsiveLine } from '@nivo/bar';
+import { ResponsiveLine } from '@nivo/line';
 import { tokens } from '../theme';
 import { mockLineData as data } from '../data/mockData';
 
