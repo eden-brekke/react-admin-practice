5a073a0610a18b0a846718e42835b820ab5cb147
Eden Brekke <eden.lorrai@gmail.com>
2023-02-28 20:11:14 -0800

    made changes to colors, fills and borders

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -4,82 +4,55 @@ import { useTheme } from '@mui/material';
 import { tokens } from '../theme';
 import { mockGeographyData as data } from '../data/mockData';
 
-const GeographyChart = () => {
+const GeographyChart = ({ isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  
+
   return (
     <ResponsiveChoropleth
         data={data}
-        features="/* please have a look at the description for usage */"
+        theme={{
+          axis:{
+            domain: {
+              line: {
+                stroke: colors.grey[100]
+              }
+            },
+            legend: {
+              text:{
+                fill: colors.grey[100]
+              }
+            },
+            ticks:{
+              line:{
+                stroke: colors.grey[100],
+                strokeWidth: 1
+              },
+              text: {
+                fill: colors.grey[100]
+              }
+            }
+          },
+          legends: {
+            text: {
+              fill: colors.grey[100]
+            }
+          }
+        }}
+        features={geoFeatures.features}
         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        colors="nivo"
         domain={[ 0, 1000000 ]}
         unknownColor="#666666"
         label="properties.name"
         valueFormat=".2s"
-        projectionTranslation={[ 0.5, 0.5 ]}
+        projectionScale={isDashboard ? 40 : 150}
+        projectionTranslation={isDashboard? [0.49, 0.6] : [ 0.5, 0.5 ]}
         projectionRotation={[ 0, 0, 0 ]}
-        enableGraticule={true}
-        graticuleLineColor="#dddddd"
-        borderWidth={0.5}
-        borderColor="#152538"
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: '#38bcb2',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: '#eed312',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-            },
-            {
-                id: 'gradient',
-                type: 'linearGradient',
-                colors: [
-                    {
-                        offset: 0,
-                        color: '#000'
-                    },
-                    {
-                        offset: 100,
-                        color: 'inherit'
-                    }
-                ]
-            }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'CAN'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'CHN'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'ATA'
-                },
-                id: 'gradient'
-            }
-        ]}
-        legends={[
-            {
+        borderWidth={1.5}
+        borderColor={colors.grey[100]}
+        legends={
+          !isDashboard ?
+          [{
                 anchor: 'bottom-left',
                 direction: 'column',
                 justify: true,
@@ -89,20 +62,20 @@ const GeographyChart = () => {
                 itemWidth: 94,
                 itemHeight: 18,
                 itemDirection: 'left-to-right',
-                itemTextColor: '#444444',
+                itemTextColor: colors.grey[100],
                 itemOpacity: 0.85,
                 symbolSize: 18,
                 effects: [
                     {
                         on: 'hover',
                         style: {
-                            itemTextColor: '#000000',
+                            itemTextColor: '#fffff',
                             itemOpacity: 1
                         }
                     }
                 ]
-            }
-        ]}
+            }] : undefined
+      }
     />
   )
 }
